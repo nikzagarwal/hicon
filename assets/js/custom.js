82e1042ee8637375d71e075962817a36c2509256
Nikhil Agarwal <nikhilagarwalassisi@gmail.com>
2021-07-05 13:44:05 +0530

    catalogues section added

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -143,3 +143,22 @@ jQuery( document ).ready(function( $ ) {
     })
  
 });
+
+var mybutton = document.getElementById("topButton");
+    
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+    
+    // When the user clicks on the button, scroll to the top of the document
+    mybutton.onclick=function(){
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+  }
